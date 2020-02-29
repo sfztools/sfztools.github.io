64f68771fa9c19cb71e881c133ff06eed9a085fd
Travis CI User <travis@example.org>
2020-02-29 13:26:09 +0000

    Release 0.3.0 (Travis build: 429)

---
+++ sfizz/api/0.3.0/_api/search/classes_a.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['scopedftz',['ScopedFTZ',['../class_scoped_f_t_z.html',1,'']]],
+  ['scopedtiming',['ScopedTiming',['../structsfz_1_1_scoped_timing.html',1,'sfz']]],
+  ['synth',['Synth',['../classsfz_1_1_synth.html',1,'sfz']]]
+];
