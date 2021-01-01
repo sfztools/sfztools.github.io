047e71f43f06d8e4ad8b859092cbdb4b8bcc68d0
redtide <redtid3@gmail.com>
2021-01-01 11:50:08 +0100

    Updated setup script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 set -e
 
 cd $PWD
@@ -10,11 +12,12 @@ if [ "$1" == "--help" ] || [ "$1" == "-h" ]; then
 	echo "Options are not mandatory, only one at a time."
 	echo "-a, --assets      Build minimized css style and js script from sources."
 	echo "-i, --install     Install Bundler and node modules using Yarn."
+	echo "-I, --incremental Runs Jekyll in incremental mode."
 	echo ""
 	exit 0
 fi
 
-if [ ! -d "node_modules" ] || [ "$1" == "-i" ] || [ "$1" == "--install" ]; then
+if [ "$1" == "-i" ] || [ "$1" == "--install" ]; then
 	gem update
 	gem install bundler
 	bundle install
@@ -25,4 +28,8 @@ if [ ! -f "assets/css/style.min.css" ] || [ "$1" == "-a" ] || [ "$1" == "--asset
 	yarn dist
 fi
 
-bundle exec jekyll serve --watch --host=0.0.0.0
+if [ "$1" == "-I" ] || [ "$1" == "--incremental" ]; then
+	incremental="-I"
+fi
+
+bundle exec jekyll serve --watch --host=0.0.0.0 $incremental
